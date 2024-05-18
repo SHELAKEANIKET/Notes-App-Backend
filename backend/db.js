ebc63312cdd92b1af74b4c8c4a30758363e23e0c
Aniket Shelake <97660224+SHELAKEANIKET@users.noreply.github.com>
2024-05-18 20:03:04 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -2,11 +2,17 @@ require('dotenv').config();
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
+  console.log('MongoDB URL:', process.env.MONGODB_URL);
 
   const uri = process.env.MONGODB_URL;
-  
+
+  if (!uri) {
+    console.error('MongoDB connection string is not defined. Please set the MONGODB_URL environment variable.');
+    process.exit(1);
+  }
+
   try {
-    await mongoose.connect(uri);
+    await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log("MongoDB connected!");
   } catch (error) {
     console.error("MongoDB connection error: ", error);
@@ -15,7 +21,3 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
-
-
-
-
