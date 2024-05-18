b38c124259b19aae73108fc8d2309bd954983664
Aniket Shelake <97660224+SHELAKEANIKET@users.noreply.github.com>
2024-05-18 19:50:30 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,12 @@
+require('dotenv').config();
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
+
+  const uri = process.env.MONGODB_URL;
+  
   try {
-    await mongoose.connect(process.env.MONGODB_URL);
+    await mongoose.connect(uri);
     console.log("MongoDB connected!");
   } catch (error) {
     console.error("MongoDB connection error: ", error);
